f936c0728d0011a5fff7fe776d0c4bda833570d4
rubenchapus <creation500@tutanota.com>
2025-03-11 15:34:30 -0400

    Landing page done.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const font = Nunito({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -23,12 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider afterSignOutUrl="/">
+      <html lang="en">
+        <body
+          className={font.className}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
