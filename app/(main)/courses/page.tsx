0368871d0ccc030f44a8be2be5ef3f96f9427f05
rubenchapus <creation500@tutanota.com>
2025-03-16 22:31:49 -0400

    Printing Database course items on page

---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,15 @@
+import { getCourses } from "@/db/queries";
+
+const CoursesPage = async () => {
+    const courses = await getCourses();
+    return (
+        <div className="h-full max-w-[912px] px-3 mx-auto">
+            <h1 className="text-2xl font-bold text-neutral-700">
+                Language Courses
+            </h1>
+            {JSON.stringify(courses)}
+        </div>
+    );
+};
+
+export default CoursesPage;
