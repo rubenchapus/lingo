a3aabb992c0130b29bfe55e9a25f08b9824746ad
rubenchapus <creation500@tutanota.com>
2025-03-16 22:56:27 -0400

    courses page done

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,4 +1,5 @@
 import { getCourses } from "@/db/queries";
+import { List } from "./list";
 
 const CoursesPage = async () => {
     const courses = await getCourses();
@@ -7,7 +8,10 @@ const CoursesPage = async () => {
             <h1 className="text-2xl font-bold text-neutral-700">
                 Language Courses
             </h1>
-            {JSON.stringify(courses)}
+            <List
+                courses={courses}
+                activeCourseId={1}
+            />
         </div>
     );
 };
